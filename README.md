50945c83716b93c7abffe126b34db98dd6f2cc7e
tjcampanella <toddcampanellajr@live.com>
2021-07-10 17:36:52 -0400

    improved example main.dart

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# secp256k1Cipher
-Encrypt and decrypt data use secp256k1
+# secp256k1_cipher
+Encrypt and decrypt data use secp256k1. This is a fork of [secp256k1Cipher](https://github.com/ipconfiger/secp256k1Cipher).
+
+
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,18 +3,22 @@ import 'package:secp256k1_cipher/secp256k1cipher.dart';
 
 void main() {
   // Create Alice's keypair
+
   var alice = generateKeyPair();
 
-  print("alice private key: " +
-      strinifyPrivateKey(alice.privateKey as ECPrivateKey));
   print(
-      "alice public key: " + strinifyPublicKey(alice.publicKey as ECPublicKey));
+    "alice private key: " +
+        strinifyPrivateKey(alice.privateKey as ECPrivateKey),
+  );
+  print(
+    "alice public key: " + strinifyPublicKey(alice.publicKey as ECPublicKey),
+  );
 
   // Create Bob's keypair
   var bob = generateKeyPair();
 
   // This is what alice wants to say to bob
-  var rawStr = 'Encrypt and decrypt data use secp256k1';
+  var rawStr = 'Encrypt and decrypt data using secp256k1';
 
   // use alic's privatekey and bob's publickey means alice says to bob
   var encMap = pubkeyEncrypt(
@@ -29,7 +33,7 @@ void main() {
 
   // Get random IV
   var iv = encMap['iv'];
-
+  print("iv: " + iv);
   // Now, you can send enc_str and IV to Bob
 
   // Use bob's privatekey and alice's publickey to decrypt alices message, for Bob to read.
